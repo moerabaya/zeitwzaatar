d34ff8c8f0a09c2d7c18783a4fec6d32d11afc08
Mohammed Rabay'a <rabaya.moe@gmail.com>
2022-11-15 10:30:30 +0200

    Initiate GraphQL

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema";
 
 const app = express();
 
@@ -6,6 +8,18 @@ app.get("/", (req, res) => {
   res.send("GraphQL Application");
 });
 
+const rootValue = {
+  hello: () => "Hi, I am Moe",
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true,
+  })
+);
 app.listen(8080, () =>
   console.log("Running server on port localhost:8080/graphql")
 );
